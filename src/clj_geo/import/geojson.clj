1bfe2f579992391b4be10879830fa350ebecd320
Vanja Komadinovic <vanjakom@gmail.com>
2019-12-16 13:00:06 +0100

    way in geojson

--- src/clj_geo/import/geojson.clj
+++ src/clj_geo/import/geojson.clj
@@ -20,3 +20,22 @@
   {
    :type "FeatureCollection"
    :features (map location->feature location-seq)})
+
+(defn way->feature
+  "Way should be defined as map containing :tags map and :locations seq.
+  Note: ways will be represented as LineString"
+  [way]
+  {
+   :type "Feature"
+   :properties (:tags way)
+   :geometry {
+              :type "LineString"
+              :coordinates (map
+                            (fn [location]
+                              [(:longitude location) (:latitude location)])
+                            (:locations way))}})
+
+(defn way-seq->geojson [way-seq]
+  {
+   :type "FeatureCollection"
+   :features (map way->feature way-seq)})
